aed0298b513229872bd27a0119bf80a70d49c870
v.buchnev <valentinbuchnev0904@gmail.com>
2024-07-20 13:53:05 +0800

    Add modal window

--- src/components/ListLocation.vue
+++ src/components/ListLocation.vue
@@ -1,19 +1,29 @@
 <script setup>
+import ModalWindow from "./ModalWindow.vue"
+import {ref} from "vue"
+
 let props = defineProps({
   streets: Array
 })
+
+let modalVisible = ref(false)
+
+function toggleModal() {
+  modalVisible.value = !modalVisible.value
+}
 </script>
 
 <template>
-  <div class="street" v-for="street in props.streets">
+  <div class="street" v-for="street in props.streets" @click="toggleModal" @toggleModal="toggleModal">
+    <ModalWindow v-if="modalVisible"/>
     {{ street.street_name }}
   </div>
 </template>
 
 <style scoped>
 .street {
-  cursor: pointer;
   margin: 10px 0;
   padding-left: 10px;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/ModalWindow.vue
@@ -0,0 +1,52 @@
+<script setup>
+let props = defineProps({
+  externalUrl: String,
+  toggleModal: Function
+})
+// export default {
+//   name: 'ExternalPage',
+//   props: {
+//     externalUrl: {
+//       type: String,
+//       // required: true
+//     }
+//   }
+// }
+</script>
+
+<template>
+  <div class="modal-window">
+    <div @click="toggleModal">Закрыть</div>
+    <div class="widget-width">
+      <iframe class="iframe"
+              src="https://vashkontrol.ru/widget?mfc=16557"
+              frameborder="0"
+              allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-window {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(95, 102, 103, 0.7);
+  z-index: 20;
+}
+
+.widget-width {
+  max-width: 1400px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 0 10px;
+}
+
+.iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
